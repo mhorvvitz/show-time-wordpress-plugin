4faba80e2764190b7bab52097a4b39c622655cd6
mhorvvitz <mhorvvitz@users.noreply.github.com>
2016-05-26 23:15:05 -0500

    Add files via upload

--- showtime.php
+++ showtime.php
@@ -9,7 +9,8 @@
  */
 
  function show_time(){
- 	date_default_timezone_set("America/Chicago");
+ 	$zone = get_option('time_zone');
+ 	date_default_timezone_set("$zone");
  	$time = date("h:i a");
  	$time = "The time is " .$time;
  	echo "<div id=show_time>$time</div>";
@@ -35,5 +36,40 @@ function show_time_css() {
 }
 
 add_action( 'wp_head', 'show_time_css' );
+
+function showtime_settings_menu() {
+	add_menu_page('Show Time Settings', 'Show Time', 'manage_options', 'showtime-settings', 'showtime_settings_page', 'dashicons-clock');
+}
+
+add_action('admin_menu', 'showtime_settings_menu');
+
+add_action( 'admin_init', 'showtime_settings' );
+
+function showtime_settings() {
+	register_setting( 'showtime_settings', 'time_zone' );
+}
+
+function showtime_settings_page() {
+?>
+<div class="wrap">
+<h2>Show Time Settings</h2>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'showtime_settings' ); ?>
+    <?php do_settings_sections( 'showtime_settings' ); ?>
+    <table class="form-table">        
+        <tr valign="top">
+        <th scope="row">Time Zone</th>
+        <td><input type="text" name="time_zone" value="<?php echo esc_attr( get_option('time_zone') ); ?>" /></td>
+        </tr>
+    </table>
+    
+    <?php submit_button(); ?>
+
+</form>
+</div>
+
+<?php
+}
 
 ?>
